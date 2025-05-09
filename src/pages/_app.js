2aff27f50700b28fdc6fffb0b7861e5e9785ca38
Amank Lingwal <amanklingwal1998@gmail.com>
2025-05-09 13:23:39 +0530

    marker

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -4,6 +4,7 @@ export default function App({ Component, pageProps }) {
   return (
     <main className="min-h-screen bg-gray-50">
       <Component {...pageProps} />
+      <h2>made by @Amank Lingwal</h2>
     </main>
   );
 }
